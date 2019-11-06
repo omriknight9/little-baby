a1cdd672d247d515ba332657abe285ec5db36e4e
Omri Shalev <omriknight9@gmail.com>
2019-11-06 11:11:55 +0200

    Add files via upload

---
+++ js/translations.js
@@ -0,0 +1,34 @@
+﻿function changeToHeb() {
+
+    lang = 2;
+    $('#langBtnHe').css('pointer-events', 'none');
+    $('#langBtnEn').css('pointer-events', 'all');
+
+    $("head").append("<link rel='stylesheet' type='text/css' href='css/main_he.css' id='hebCss'/>");
+    $('title').html('תינוק קטן');
+
+    $('h1').hide().html('תינוק קטן').fadeIn('slow');
+
+    $('header p').hide().html('זו' + "<span id='girlSpan'> בת!</span>").fadeIn('slow');
+
+    $('.popupBtn').hide().html('סגור').fadeIn('slow');
+
+    showBaby();
+}
+
+function changeToEng() {
+    lang = 1;
+    $('#langBtnEn').css('pointer-events', 'none');
+    $('#langBtnHe').css('pointer-events', 'all');
+
+    $('#hebCss').remove();
+    $('title').html('Little Baby');
+
+    $('h1').hide().html('Little Baby').fadeIn('slow');
+
+    $('header p').hide().html("It's A"  + "<span id='girlSpan'> Girl!</span>").fadeIn('slow');
+    
+    $('.popupBtn').hide().html('Close').fadeIn('slow');
+
+    showBaby();
+}
