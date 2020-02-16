f4940e91f5ea916dfa46ba6eefd0e6bdd041e1a7
Omri Shalev <omriknight9@gmail.com>
2020-02-16 16:05:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 let littleBaby = [];
 let gallery = [];
 let videos = [];
+let laborBagList = [];
 let counter = 1;
 let eventsCounter = 1;
 let galleryCounter = 1;
@@ -92,6 +93,7 @@ $(document).ready(function (event) {
         $('.spinnerWrapper').hide();
         $('.searchContainer').show();
         $('#eventContainer').css('display', 'flex');
+        $('#listImg').show();
     }, 1500);
 });
 
@@ -125,7 +127,6 @@ function startImgInterval() {
             if (lang == 1) {
                 $('#weekNum').html('Week: ' + weekImgCounter);
             } else {
-                console.log('LANG 2!!!')
                 $('#weekNum').html('שבוע: ' + weekImgCounter);
             }
             $('#weekImg').attr('src', './images/weekPhotos/' + weekImgCounter + '.jpeg');
@@ -253,7 +254,87 @@ function loadJson() {
         $('h2').show();
         $('#weeksCount').show();
         $('#monthCount').show();
+        $('#listImg').show();
     }, 1000)
+}
+
+function loadList() {
+
+    $.get('./lists/laborBag.txt', function (data) {
+        laborBagList.push(JSON.parse(data));
+        setTimeout(function () {
+            buildList('listWrapper', $('#listContainer'), laborBagList);
+        }, 500);
+        $('html, body').animate({ scrollTop: $('#listContainer').position().top -210 }, 'slow');
+    });
+}
+
+function buildList(wrapper, containter, arr) {
+
+    let laborBag  = arr[0].laborBag;
+
+    if (lang == 1) {
+        headerText = 'Labor Bag';
+    } else {
+        headerText = 'תיק לידה';
+    }
+
+    let header = $('<h2>', {
+        text: headerText
+    }).appendTo(containter);
+
+    let listWrapper = $('<div>', {
+        class: 'listWrapper'
+    }).appendTo(containter);
+
+    for (var i = 0; i < laborBag.length; i++) {
+
+        let name;
+
+        if (lang == 1) {
+            name = laborBag[i].name;
+        } else {
+            name = laborBag[i].nameHeb;
+        }
+
+        let listItemWrapper = $('<div>', {
+            class: 'listItemWrapper',
+            numId: laborBag[i].id,
+            name: laborBag[i].name,
+            click: function() {
+                let star = $(this).find('.listItemCheck');
+                if (star.attr('src') == '../images/unchecked.png') {
+                    localStorage.setItem('itemId' + $(this).attr('numId'), $(this).attr('numId'));
+                    $(this).find('.listItemCheck').attr('src', '../images/checked.png');
+                } else {
+                    localStorage.removeItem('itemId' + $(this).attr('numId'), $(this).attr('numId'));
+                    $(this).find('.listItemCheck').attr('src', '../images/unchecked.png');
+                }
+            }
+        }).appendTo(listWrapper);
+
+        let listItem = $('<p>', {
+            class: 'listItem',
+            text: laborBag[i].id + '. ' + name,
+        }).appendTo(listItemWrapper);
+
+        let listItemCheck = $('<img>', {
+            class: 'listItemCheck',
+            src: '../images/unchecked.png'
+        }).appendTo(listItemWrapper);
+
+        $.each($('.listItemWrapper'), function (key, value) {
+            let data = localStorage.getItem('itemId' + $(value).attr('numId'));
+            if (data) {
+                let that = $(this).find($('.listItemCheck'));
+                if ($(value).attr('numId') === data) {
+                    $(that).attr('src', './images/checked.png');
+                } else {
+                    $($(that).find($('.star')).attr('src', './images/unchecked.png'));
+                }
+            }
+        });
+    }
 }
 
 Date.testTime = function(date1, date2) {
